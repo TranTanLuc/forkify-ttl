1761f6839e42359f5fda45c881d558c9c156875d
Tran Tan Luc <luc972000@gmail.com>
2022-11-24 23:07:43 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'adf48c50-837d-4213-8f26-221983dc3a77';
+export const MODAL_CLOSE_SEC = 2.5;
